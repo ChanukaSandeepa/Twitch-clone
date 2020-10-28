c6d77eb161c4e4e97d9df548ccec19d798c63c17
Chanuka <chanursandeepa@gmail.com>
2020-10-29 01:07:55 +0530

    Setting up and header component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img className="header_leftLogo" src="https://pngimg.com/uploads/twitch/twitch_PNG6.png" />
+                <h2>Following</h2>
+                <h2>Browse</h2>
+                <div className="header_vertical"></div>
+            </div>
+            <div className="header-center">
+
+            </div>
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+}
