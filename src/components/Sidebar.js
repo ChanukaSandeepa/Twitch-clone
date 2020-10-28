553f36453034681f68de8c784c347eab461dc6af
Chanuka <chanursandeepa@gmail.com>
2020-10-29 03:41:27 +0530

    sidebar search container

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,14 @@ import Channel from './Channel'
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <p>FOLLOWED CHANNEL</p>
-            <Channel/>
-            <p>RECOMMENDED CHANNEL</p>
+            <div style={{height : '90%', width : '100%'}}>
+                <p>FOLLOWED CHANNEL</p>
+                <Channel/>
+                <p>RECOMMENDED CHANNEL</p>
+            </div>
+            <div style={{height : '10%', width : '100%'}} className="serch-container">
+                <input type="text" placeholder="Search"/>
+            </div>
         </div>
     )
 }
